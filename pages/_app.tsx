02d618271e846aa5da8104f274d61305395f8a2b
Jorge Ariel Castillo <j.a.castillo.jc@gmail.com>
2023-06-30 15:40:09 -0300

    create context and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+import { UIProvider } from '@/context/ui';
 import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { lightTheme, darkTheme } from '../themes';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <Component {...pageProps} />      
+      </ThemeProvider>        
+    </UIProvider>
+
+  )
 }
